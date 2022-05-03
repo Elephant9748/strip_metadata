81e99299f04d14a7b00e2579541f0870d54ba19f
bigort <adytamasurya@protonmail.com>
2022-05-04 03:45:58 +0700

    preinstalled setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir img
+mkdir img/ori
+mkdir ori-BK
+mkdir out
+mkdir qrcode
+
+# require python3 and pip
+pip install -r requirement.txt
+
+sudo apt install imagemagick-6.q16  
+# sudo apt install imagemagick-6.q16hdri              
+# sudo apt install graphicsmagick-imagemagick-compat
